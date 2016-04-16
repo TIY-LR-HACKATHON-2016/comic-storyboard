93ff0938e37565cfa48149bb60237f3e55399067
Brian Stickney <brianchase055@gmail.com>
2016-04-15 20:46:33 -0500

    ComicBook Class

---
+++ server/ComicStoryBook/ComicStoryBook.Web/Models/ComicBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicStoryBook.Web.Models
+{
+    public class ComicBook
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Tile> Tiles { get; set; }= new List<Tile>();
+    }
+}
