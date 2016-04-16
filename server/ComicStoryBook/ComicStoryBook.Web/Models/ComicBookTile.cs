89dcbbca93b792ca162fdfdd429684dc1344b515
Brian Stickney <brianchase055@gmail.com>
2016-04-15 20:25:11 -0500

    ComicBookTile Model

---
+++ server/ComicStoryBook/ComicStoryBook.Web/Models/ComicBookTile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Web;
+
+namespace ComicStoryBook.Web.Models
+{
+    public class ComicBookTile
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+        
+        
+            
+        }
+    }
+}
