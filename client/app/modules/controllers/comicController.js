57964b53a5bdb76be6d673caa2aa0cd233115406
jbooher <john.c.booher@gmail.com>
2016-04-16 14:11:01 -0500

    Basic stylings done.  Adding the functionality to the buttons.

--- client/app/modules/controllers/comicController.js
+++ client/app/modules/controllers/comicController.js
@@ -1,6 +1,24 @@
 class ComicController {
-  constructor() {
+  constructor($http) {
+    this._$http = $http;
+    this.show = false;
 
+    this.getData();
+  }
+
+  getData() {
+    this._$http
+      .get(`http://tiycomicbook.azurewebsites.net/comicbooks/index`)
+      .then((response) => {
+        console.log(response);
+        this.tiles = response.data;
+        console.log(this.tiles);
+      });
+  }
+
+  showButtons() {
+    console.log("trying to show!");
+    this.show = !this.show;
   }
 }
 
