99e7b4bd3aebfa17c1eaef9354e6afc0d643a6db
John Keisker <jkeisker@me.com>
2016-04-15 22:15:26 -0500

    carousel setup.

--- client/app/modules/controllers/tileController.js
+++ client/app/modules/controllers/tileController.js
@@ -1,6 +1,40 @@
 class TileController {
-  constructor() {
+  constructor($http) {
+    this._$http = $http;
 
+    this.carousel = [
+      'http://www.placecage.com/100/200',
+      'http://www.placecage.com/c/100/200',
+      'http://www.placecage.com/200/200'];
+    this.carouselImage = 0;
+
+    this.getData();
+
+  }
+  getData() {
+    this._$http
+      .get(``)
+      .then((respnse) => {
+        console.log(response);
+      });
+  }
+
+  previousImage() {
+    if (this.carouselImage === 0) {
+      this.carouselImage = this.carousel.length - 1;
+    }
+    else {
+      this.carouselImage -= 1;
+    }
+  }
+
+  nextImage() {
+    if (this.carouselImage === this.carousel.length - 1) {
+      this.carouselImage = 0;
+    }
+    else {
+      this.carouselImage += 1;
+    }
   }
 }
 
